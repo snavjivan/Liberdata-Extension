d67900d5509d90cdd7914c412f6e0eba353f5e98
Andreas Paljug <andreaspaljug@gmail.com>
2019-09-07 09:54:13 -0400

    postHistory setup.

--- background.js
+++ background.js
@@ -4,10 +4,24 @@ chrome.browserAction.onClicked.addListener(function(tab) {
     chrome.history.search({text: '', maxResults: 10}, function(data) {
         data.forEach(function(page) {
             //send message to backend....
-            const url = "http://localhost:3000";
-            const data = {"message": page};
+            const yourUrl = "http://localhost:5000/api/postHistory";
+
             /*$.post(url + 'history1',
             data );*/
+            var xhr = new XMLHttpRequest();
+        xhr.open("POST", yourUrl, true);
+        xhr.setRequestHeader('Content-Type', 'application/json');
+        xhr.send(JSON.stringify({
+            value: page
+        }));
+/*
+        // Make a simple request:
+        chrome.runtime.sendMessage(activeTab.id,  {openUrlInEditor: url},
+          function(response) {
+            console.log("yuh?")
+            if (!response.success)
+              handleError(url);
+          });*/
 
             // Send a message to the active tab
             chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
