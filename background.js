6012cf13bea77b6569795f16880571e44590a3db
Andreas Paljug <andreaspaljug@gmail.com>
2019-09-07 03:18:22 -0400

    started setting up post to react backend from chrome extension background.js

--- background.js
+++ background.js
@@ -1,6 +1,14 @@
+
 chrome.browserAction.onClicked.addListener(function(tab) {
+  console.log("begin background stuff");
     chrome.history.search({text: '', maxResults: 10}, function(data) {
         data.forEach(function(page) {
+            //send message to backend....
+            const url = "http://localhost:3000";
+            const data = {"message": page};
+            /*$.post(url + 'history1',
+            data );*/
+
             // Send a message to the active tab
             chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
                 var activeTab = tabs[0];
