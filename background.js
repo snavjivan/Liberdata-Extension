26288fe4f44f4221d4df9a8147c15ad5cfdf66e9
Marc D'Mello <marcd2000@gmail.com>
2019-09-07 12:14:58 -0400

    Turned into list

--- background.js
+++ background.js
@@ -2,32 +2,24 @@
 chrome.browserAction.onClicked.addListener(function(tab) {
   console.log("begin background stuff");
     chrome.history.search({text: '', maxResults: 10}, function(data) {
+        pages = [];
         data.forEach(function(page) {
-            //send message to backend....
-            const yourUrl = "http://localhost:5000/api/postHistory";
+            pages.push(page);
+        });
+        //send message to backend....
+        const yourUrl = "http://localhost:5000/user-api/postHistory";
 
-            /*$.post(url + 'history1',
-            data );*/
-            var xhr = new XMLHttpRequest();
+        var xhr = new XMLHttpRequest();
         xhr.open("POST", yourUrl, true);
-        xhr.setRequestHeader('Content-Type', 'application/json');
+        xhr.setRequestHeader('Content-Type', 'application/json;charset=UTF-8');
         xhr.send(JSON.stringify({
-            value: page
+            value: pages
         }));
-/*
-        // Make a simple request:
-        chrome.runtime.sendMessage(activeTab.id,  {openUrlInEditor: url},
-          function(response) {
-            console.log("yuh?")
-            if (!response.success)
-              handleError(url);
-          });*/
 
-            // Send a message to the active tab
-            chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-                var activeTab = tabs[0];
-                chrome.tabs.sendMessage(activeTab.id, {"message": page});
-            });
+        // Send a message to the active tab
+        chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+            var activeTab = tabs[0];
+            chrome.tabs.sendMessage(activeTab.id, {"message": pages});
         });
     });
 });
