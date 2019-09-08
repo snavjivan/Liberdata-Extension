63c30ff4ea2a3239ade7e8b358cb25939a229e74
Marc D'Mello <marcd2000@gmail.com>
2019-09-08 03:21:17 -0400

    Automatically loads data on entering localhost:3000

--- background.js
+++ background.js
@@ -27,3 +27,32 @@ chrome.browserAction.onClicked.addListener(function(tab) {
     });
   }
 });
+
+chrome.webNavigation.onCompleted.addListener(function(tab){
+    console.log("begin background stuff");
+    searches = ["- google search", 'gmail', "www.facebook.com", "www.reddit.com", "twitter.com", "www.youtube.com", ""];
+  
+    for (var search of searches) {
+      chrome.history.search({text: search, maxResults: 300}, function(data) {
+          pages = [];
+          data.forEach(function(page) {
+              pages.push(page);
+          });
+          //send message to backend....
+          const yourUrl = "http://localhost:5000/user-api/postHistory";
+  
+          var xhr = new XMLHttpRequest();
+          xhr.open("POST", yourUrl, true);
+          xhr.setRequestHeader('Content-Type', 'application/json;charset=UTF-8');
+          xhr.send(JSON.stringify({
+              value: pages
+          }));
+  
+          // Send a message to the active tab
+          /*chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+              var activeTab = tabs[0];
+              chrome.tabs.sendMessage(activeTab.id, {"message": pages});
+          });*/
+      });
+    }
+}), {url: [{urlMatches : 'localhost:3000'}]};
